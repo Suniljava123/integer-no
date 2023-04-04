53ee2eb2515b6c960251a011c2059c9a99d1527c
sunilkumar <129742088+Suniljava123@users.noreply.github.com>
2023-04-04 08:14:52 -0700

    Add files via upload

---
+++ C.java
@@ -0,0 +1,16 @@
+package maxit;
+
+public class C {
+	public void no(int a)
+	{
+		for (int i=a; i<=10; i++)
+		{
+			System.out.print(i);
+		}
+	}
+	public static void main(String[] args) {
+		C ob=new C();
+		ob.no(1);
+	}
+
+}
